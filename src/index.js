851ead60e3ac1d13791f7e3935e6ff5b358f91a3
Jorge del Casar <jorge.casar@gmail.com>
2019-05-17 10:07:28 +0200

    add routing support

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import './styles.css';
-import './views/my-app.js';
 import { Router } from '@vaadin/router';
 
 window.addEventListener('load', () => {
@@ -12,16 +11,38 @@ function initRouter() {
   router.setRoutes([
      {
       path: '/',
-      component: 'my-app'
-    }
-    // {
-    //   path: '/',
-    //   component: 'view-a'
-    // },
-    // {
-    //   path: '/view-b',
-    //   component: 'view-b',
-    //   action: () => import(/* webpackChunkName: "viewb" */ './views/view-b')
-    // }
+      component: 'my-app',
+      action: () => import(/* webpackChunkName: "app" */ './views/my-app')
+    },
+    {
+      path: '/view-a',
+      component: 'view-a',
+      action: () => import(/* webpackChunkName: "viewa" */ './views/view-a')
+    },
+    {
+      path: '/view-b',
+      component: 'view-b',
+      action: () => import(/* webpackChunkName: "viewb" */ './views/view-b')
+    },
+    {
+      path: '/view-c',
+      component: 'view-c',
+      action: () => import(/* webpackChunkName: "viewc" */ './views/view-c')
+    },
+    {
+      path: '/view-ab',
+      component: 'view-ab',
+      action: () => import(/* webpackChunkName: "viewab" */ './views/view-ab')
+    },
+    {
+      path: '/view-bc',
+      component: 'view-bc',
+      action: () => import(/* webpackChunkName: "viewbc */ './views/view-bc')
+    },
+    {
+      path: '/view-ac',
+      component: 'view-ac',
+      action: () => import(/* webpackChunkName: "viewac" */ './views/view-ac')
+    },
   ]);
 }
--- src/views/view-ab.js
+++ src/views/view-ab.js
@@ -1,18 +1,16 @@
 import { LitElement, html } from 'lit-element';
 import './view-a';
 import './view-b';
-import './view-c';
 
 // Extend the LitElement base class
-class MyApp extends LitElement {
+class ViewAB extends LitElement {
   render(){
     return html`
       <!-- template content -->
       <view-a></view-a>
       <view-b></view-b>
-      <view-c></view-c>
     `;
   }
 }
 // Register the new element with the browser.
-customElements.define('my-app', MyApp);
+customElements.define('view-ab', ViewAB);
